d99317070768aacedf1053c15bc17067a5397887
Joseph Lev-Tov <jtlevtov@gmail.com>
2021-03-17 12:38:46 -0600

    First attempt at setting up the database.

--- duckBot.py
+++ duckBot.py
@@ -3,6 +3,17 @@ import asyncio
 # minecraft
 import discord
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class DiscordUser(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    timesDucked = db.Column(db.INTEGER, default=1)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 # Contains the classes and functions used to log the duck counts to a file, like a database
 import duckLogger
@@ -20,6 +31,14 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 
+def inDatabase(name, users):
+    for user in users:
+        if user.name == name:
+            return user.id
+    return "no user with that name"
+
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -31,7 +50,8 @@ async def on_message(message):
         if message.channel.name == "temple":
             return
         send = ""
-        users = duckLogger.getInfo()
+        users = DiscordUser.query.all()
+        # users = duckLogger.getInfo()
         for user in users:
             send = send + f'{user.name} has devoted {user.timesDucked} times\n'
         await message.channel.send(send)
@@ -44,20 +64,32 @@ async def on_message(message):
         if message.author == client.user:
             return
 
-        users = duckLogger.getInfo()
+        # users = duckLogger.getInfo()
+        users = DiscordUser.query.all()
         # for user in users:
         #     print(user.formatReturn())
-        userIndex = duckLogger.inUserList(message.author.name, users)
-        print(userIndex)
+        # userID = duckLogger.inUserList(message.author.name, users)
 
-        if userIndex == "no user with that name":
+        userID = inDatabase(message.author.name)
+        print(userID)
+
+        if userID == "no user with that name":
             print("|||||||")
-            user = duckLogger.User(message.author.name, 1)
-            users.append(user)
+            newUser = DiscordUser(name=message.author.name)
+            db.session.add(newUser)
+            db.session.commit()
+
+
+            # user = duckLogger.User(message.author.name, 1)
+            # users.append(user)
         else:
             print("-------")
-            users[userIndex].timesDucked = int(users[userIndex].timesDucked) + 1
-            print(users[userIndex].timesDucked)
+            user = DiscordUser.query.get_or_404(userID)
+            user.timesDucked += 1
+            db.session.commit()
+
+            # users[userID].timesDucked = int(users[userID].timesDucked) + 1
+            # print(users[userID].timesDucked)
         duckLogger.writeInfo(users)
         print('hi')
     else:
